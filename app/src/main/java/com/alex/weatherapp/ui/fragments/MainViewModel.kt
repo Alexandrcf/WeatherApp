94aa3e33751f2b5ef48d43200107c28b43096a31
Alexandr <alexandrcf@mail.ru>
2023-04-17 19:38:46 +0300

    Last changes

--- app/src/main/java/com/alex/weatherapp/ui/fragments/MainViewModel.kt
+++ app/src/main/java/com/alex/weatherapp/ui/fragments/MainViewModel.kt
@@ -4,20 +4,29 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.alex.weatherapp.domain.models.JsonDataClass
+import com.alex.weatherapp.domain.useCase.GetTemperatureUseCase
 import kotlinx.coroutines.*
 
-class MainViewModel() : ViewModel() {
+class MainViewModel(
+    private val getTemperatureUseCase: GetTemperatureUseCase
+) : ViewModel() {
 
     private val coroutineScope = CoroutineScope(Dispatchers.Default + CoroutineName("My coroutine scope"))
-
 
     val temperatureApiLiveData: MutableLiveData<List<JsonDataClass>> by lazy {
         MutableLiveData<List<JsonDataClass>>()
     }
 
-    fun getBotsFromApi() {
+    fun getTemperatureFromApi() {
         viewModelScope.launch {
-
+            try {
+                //TODO
+                //temperatureApiLiveData.value = getTemperatureUseCase.execute()
+            }
+            catch (e: Exception)
+            {
+                e.toString()
+            }
         }
     }
 
