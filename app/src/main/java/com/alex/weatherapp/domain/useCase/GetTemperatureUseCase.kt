f7220eef6561d518385a3ac376db5b4bd9befc3c
Alexandr <alexandrcf@mail.ru>
2023-04-19 22:22:58 +0300

    Rework dependencies

--- app/src/main/java/com/alex/weatherapp/domain/useCase/GetTemperatureUseCase.kt
+++ app/src/main/java/com/alex/weatherapp/domain/useCase/GetTemperatureUseCase.kt
@@ -1,9 +1,9 @@
 package com.alex.weatherapp.domain.useCase
 
 import com.alex.weatherapp.domain.models.JsonDataClass
-import com.alex.weatherapp.domain.repository.RepositoryWeather
+import com.alex.weatherapp.domain.repository.WeatherRepository
 
-class GetTemperatureUseCase(private val repositoryWeather: RepositoryWeather) {
+class GetTemperatureUseCase(private val repositoryWeather: WeatherRepository) {
 
     suspend fun execute(): JsonDataClass? {
         return repositoryWeather.getTemperature()
